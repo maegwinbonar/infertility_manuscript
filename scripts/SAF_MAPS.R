0e1c55f30ba5f9e40f6d3c84756d77fe6b45e9a5
Maegwin Bonar <maegwinbonar@gmail.com>
2025-07-23 15:47:47 -0400

    updated maps script
    added input definition

--- scripts/SAF_MAPS.R
+++ scripts/SAF_MAPS.R
@@ -14,136 +14,16 @@ library(mapview)
 library(scatterpie)
 
 ######################################################
-######################################################
-
-oceania <- read.table("count_geo_Populations_combinedSAF_chr8-116372254.tsv", sep="\t", header = T)
-
-oceania <- oceania %>%
-  pivot_longer(
-    cols = c(SAF_A, SAF_G),
-    names_to = "SAF",
-    values_to = "Percent"
-  )
-
-oceania
-
-population_order <- c("Sepik","Goroka","Kove","Nakanai-Mangseng","Ata-Mamusi","Melamela","Baining-Kagat","Baining-Mali","Lavongai-Mussau",
-                      "Nailik-Notsi-Tigak","Saposa","Nasioi","Vella-Lavella","Malaita","Bellona", "Rennell","Santa-Cruz","Tikopia")
-
-pie_chart_list <- list()
-
-for (group in population_order) {
-  group_df <- oceania %>%
-    filter(AnalysisGroup == group)
-  
-  pie_chart <- ggplot(group_df, aes(x = 1, y = Percent, fill = SAF)) +
-    geom_bar(stat = "identity", width = 1, color = "grey20") +
-    coord_polar(theta = "y") +
-    scale_fill_manual(values = c("SAF_A" = "#FEBF0F", "SAF_G" = "#F15B22")) +  
-    labs(title = group) +
-    theme_void() +
-    theme(legend.position = "none")
-  
-  pie_chart_list[[group]] <- pie_chart
-}
-
-pie_chart_grid <- wrap_plots(pie_chart_list, nrow = 3)
-pie_chart_grid
-
-
-
-#######################################
-# GLOBAL 
-#######################################
-
-
-global <- read.table("count_geo_Regions_chr8-116372254.tsv", sep="\t", header = T)
-
-global <- global %>%
-  pivot_longer(
-    cols = c(SAF_A, SAF_G),
-    names_to = "SAF",
-    values_to = "Percent"
-  )
-
-print(global)
-
-population_order <- c("AFR","AMR","CSA","EAS","EUR","ISEA","MDE","OCN")
-
-pie_chart_list <- list()
-
-for (group in population_order) {
-  group_df <- global %>%
-    filter(Region == group)
-  
-  pie_chart <- ggplot(group_df, aes(x = 1, y = Percent, fill = SAF)) +
-    geom_bar(stat = "identity", width = 2, color = "grey20") +
-    coord_polar(theta = "y") +
-    scale_fill_manual(values = c("SAF_A" = "#FEBF0F", "SAF_G" = "#F15B22")) +  
-    labs(title = group) +
-    theme_void() +
-    theme(legend.position = "none")
-  
-  pie_chart_list[[group]] <- pie_chart
-}
-
-pie_chart_grid <- wrap_plots(pie_chart_list, nrow = 3)
-pie_chart_grid
-
-
-
-df_sf <- global %>%
-  st_as_sf(coords = c("Latitude", "Longitude"), crs=4326)
-
-
-world <- ne_countries(scale = "medium", returnclass = "sf", country = NULL)
-class(world)
-
-
-ggplot(data = world) +
-  geom_sf() +
-  geom_point(data = global, aes(x = Longitude, y = Latitude, 
-                                fill=Region),
-             position=position_jitter(h=0.1, w=0.1),
-             shape = 21, alpha = 0.7, size = 3) +
-  ggrepel::geom_label_repel(data = global, 
-                            aes(x = Longitude, y = Latitude,
-                                label = Region), 
-                            colour = "black",
-                            size = 2,
-                            #segment.colour = "darkgrey",
-                            min.segment.length = 0, 
-                            max.overlaps = 20) +
-  coord_sf(xlim = c(-125, 170), ylim = c(-50, 88), expand = TRUE, 
-           lims_method = c("cross"))
-
-
-ggplot(data = world) +
-  geom_sf() +
-  geom_scatterpie(data = global, aes(x = Longitude, y = Latitude),
-                  cols = c("SAF_A", "SAF_G"), 
-                  alpha = 0.85, 
-                  col = NA, 
-                  stroke = 1) +
-  scale_fill_manual(values = c("#99C24D", "#4BA5BE"), 
-                    labels = c("Ancestral(A)", "Derived(G)")) +
-  ggrepel::geom_text_repel(data = global,
-                           aes(x = Longitude, y = Latitude,
-                               label = Region),
-                           colour = "black",
-                           size = 3,
-                           segment.size = 0.5,
-                           segment.color = 'black',
-                           segment.alpha = 0.5,
-                           force = 0.5,
-                           min.segment.length = 0,
-                           max.overlaps = Inf) +
-  labs(fill = "Allele") +
-  coord_sf(xlim = c(-125, 170), ylim = c(-50, 88), expand = TRUE, 
-           lims_method = c("cross"))
-
-######################################################
-# Maegwin's Data
+#Input: .csv file with the following columns: 
+  #POP	- population
+  #REF	- reference allele frequency
+  #ALT  - alternate allele frequency
+  #Latitude	-latitutde centroid from population samples
+  #Longitude	- longitude centroid from population samples
+  #rsid - rsid of lead variant
+  #A1 - reference allele
+  #A2 - alternate allele
+  #effect - effect allele defined in GWAS
 ######################################################
 
 global <- read.table("rsid_allele_freq.csv", sep=",", header = T)
